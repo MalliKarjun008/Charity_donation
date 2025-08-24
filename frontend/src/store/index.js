945e68f8b354829c74e469d3c4e7b2ab2b99067c
malingaray <jmalingaray@gmail.com>
2025-08-24 17:51:15 +0530

    checking the git push from another branch

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,3 +5,5 @@ import { alertReducers } from "./alert/alertSlice";
 export const store = configureStore({
   reducer: { auth: authReducer, alert: alertReducers },
 });
+
+// some changes to check git
